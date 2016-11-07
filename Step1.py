c3febc22e7d34bf99552cfcde7d20e82f94506b3
ljBenson21 <lbenson18@my.whitworth.edu>
2016-11-07 14:47:16 -0800

    Add files via upload

---
+++ Step1.py
@@ -0,0 +1,8 @@
+import requests
+import json
+json_string = '{"token" : "32003a47cded737d82e38ec1fc226905", "github" : "https://github.com/ljBenson21/Code-2040-Challenge.git"}'
+parsed_json = json.loads(json_string)
+headers = {'content-type' : 'application/json'}
+r = requests.post('http://challenge.code2040.org/api/register', json = parsed_json, headers = headers)
+print (r.content)
+
---
+++ Step2.py
@@ -0,0 +1,6 @@
+str = 'yqmczewr'
+rev = ""
+for c in str:
+	rev = c + rev
+
+print(rev)
