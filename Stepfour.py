2b10220c0bae2101aeb04f83e616ab28107427e3
ljBenson21 <lbenson18@my.whitworth.edu>
2016-11-10 11:41:00 -0800

    This is my original code that wasn't working

---
+++ Stepfour.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+
+def code2040step4():
+	json_string = '{"token" : "32003a47cded737d82e38ec1fc226905"}'
+	parsed_json = json.loads(json_string)
+	headers = {'content-type': 'application/json'}
+	r = requests.post('http://challenge.code2040.org/api/prefix', json = parsed_json, headers = headers)
+	print (r.text)
+	parsed_json = json.loads(r.text)
+	list = parsed_json["array"]
+	substr = parsed_json["prefix"]
+	noPrfArray = []
+	for string in list:
+		if string.find(substr) == -1:
+			noPrfArray.append(string)
+
+	
+	str = "".join(noPrfArray)
+	json_string = '{"token" : "32003a47cded737d82e38ec1fc226905", "array" : "'+str+'"}'
+	parsed_json = json.loads(json_string)
+	r = requests.post('http://challenge.code2040.org/api/prefix/validate', json = parsed_json, headers = headers)
+	print(r.text)
+	
+	
+	
+	
+code2040step4()
