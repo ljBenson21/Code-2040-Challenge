b846ba5070deabd85ca7e4d027ca90a3c072c203
ljBenson21 <lbenson18@my.whitworth.edu>
2016-11-11 01:21:37 -0800

    Update Step1.py

--- Step1.py
+++ Step1.py
@@ -1,8 +1,11 @@
 import requests
 import json
+# I made a JSON string variable so that I could pass in my token and my url to github
 json_string = '{"token" : "32003a47cded737d82e38ec1fc226905", "github" : "https://github.com/ljBenson21/Code-2040-Challenge.git"}'
-parsed_json = json.loads(json_string)
-headers = {'content-type' : 'application/json'}
-r = requests.post('http://challenge.code2040.org/api/register', json = parsed_json, headers = headers)
+#
+sendThing = json.loads(json_string) # I loaded the JSON string variable into a variable called sendthing... couldnt think of something creative 
+headers = {'content-type' : 'application/json'} # so that I could read what I posted to the web address
+# posted to the website and got confirmation that I was successfully registered
+r = requests.post('http://challenge.code2040.org/api/register', json = sendThing, headers = headers) 
 print (r.content)
 
