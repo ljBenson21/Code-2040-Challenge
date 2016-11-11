c2bafb4294a1a54c5860b0f1bfe1d66b69cb846b
ljBenson21 <lbenson18@my.whitworth.edu>
2016-11-11 01:43:18 -0800

    Update Step5.py

--- Step5.py
+++ Step5.py
@@ -1,7 +1,7 @@
 import requests 
 import json
 import datetime
-
+# this one took a bit longer
 def code2040stepfive():
 	json_string = '{"token" : "32003a47cded737d82e38ec1fc226905"}' 
 	parsed_json = json.loads(json_string)
@@ -9,20 +9,20 @@ def code2040stepfive():
 	r = requests.post('http://challenge.code2040.org/api/dating', json = parsed_json, headers = headers)
 	print(r.text)
 	parsed_json = json.loads(r.text)
-	date = parsed_json["datestamp"]
+	date = parsed_json["datestamp"] #first parse; I found that I could split up the individual items in the original ISO 8601
 	year = date[:4]
 	month = date[5:7]
 	day = date[8:10]
 	hours = date[11:13]
 	minutes = date[14:16]
 	sec = date[17:19]
-	
+	# after splitting them I put them all together into given time; also putting them into integers instead of strings
 	givenTime = datetime.datetime(int(year), int(month) , int(day), int(hours), int(minutes), int(sec))
 	print(givenTime)
-	sec2Add= parsed_json["interval"]
-	newTime = givenTime + datetime.timedelta(seconds = sec2Add)
-	print (newTime.isoformat())
-	NewDateStamp = newTime.isoformat() + "Z"
+	sec2Add= parsed_json["interval"] # made a seconds to add variable holding the interval
+	newTime = givenTime + datetime.timedelta(seconds = sec2Add) #now that I have the data and interval separate, I add the interval
+	print (newTime.isoformat()) 
+	NewDateStamp = newTime.isoformat() + "Z"# turn newTime back into a string object using isoformat() that I found on the interweb with Z added
 	
 	json_string = {"token" : "32003a47cded737d82e38ec1fc226905", "datestamp" : NewDateStamp}
 	print(json_string)
