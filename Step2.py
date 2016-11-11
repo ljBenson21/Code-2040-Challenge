7c556a8cbcdef5900b0aeb9eb349dceb5b6e7d2c
ljBenson21 <lbenson18@my.whitworth.edu>
2016-11-11 01:24:10 -0800

    Update Step2.py

--- Step2.py
+++ Step2.py
@@ -4,14 +4,14 @@ import json
 
 def code2040StepTwo():
 	json_string = '{"token" : "32003a47cded737d82e38ec1fc226905"}'
-	parsed_json = json.loads(json_string)
+	stringThing = json.loads(json_string)
 	headers = {'content-type': 'application/json'}
-	r = requests.post('http://challenge.code2040.org/api/reverse', json = parsed_json, headers = headers)
+	r = requests.post('http://challenge.code2040.org/api/reverse', json = stringThing, headers = headers)
 	print(r.content)
-	str = r.text
-	rev = ""
+	str = r.text # created a string of the text sent to me
+	rev = "" # I created a string called reverse that is set to nothing
 	print(str)
-	for c in str:
+	for c in str: # after each loop the character c is added into the reverse string
 		rev = c + rev
 	
 	json_string = '{"token" : "32003a47cded737d82e38ec1fc226905" , "string" : "'+rev+'"}' 
